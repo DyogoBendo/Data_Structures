822c001eaefe8037ef57ebf19ada62a62cb76e46
DyogoBendo <dyogoromagnabendo@gmail.com>
2020-11-27 17:27:48 -0300

    Testes com as listas

---
+++ stack.py
@@ -0,0 +1,63 @@
+from linked_list import DoublyLinkedList
+
+# Implementing stack with linked_list
+
+
+class Stack:
+    def __init__(self, element):
+        self.list = DoublyLinkedList()
+        self.push(element)
+
+    def size(self):
+        return self.list.size()
+
+    def is_empty(self):
+        return self.size() == 0
+
+    def push(self, element):
+        self.list.addLast(element)
+
+    def pop(self):
+        return self.list.removeLast()
+
+    def peek(self):
+        if self.is_empty():
+            raise Exception("Empty Stack")
+        return self.list.peekLast()
+
+    def __repr__(self):
+        return self.list.__repr__()
+
+
+if __name__ == '__main__':
+    stack = []
+    # pushing elements to the stack
+    stack.append('a')
+    stack.append('b')
+    stack.append('c')
+
+    print(stack)
+
+    # poping elements
+    stack.pop()
+    stack.pop()
+
+    print(stack)
+
+    # size of the stack
+    print(len(stack))
+
+    # using the class
+
+    another_stack = Stack(10)
+
+    another_stack.push(5)
+
+    print(another_stack)
+
+    another_stack.pop()
+
+    print(another_stack)
+
+
+
